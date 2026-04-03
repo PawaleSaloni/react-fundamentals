66820b94d10063b33a2ccf13a8b7008745fb45bf
salonipawale <pawalesaloni123@gmail.com>
2026-04-03 22:15:06 +0530

    Updated App.js file

--- transflower-store/src/Product.js
+++ transflower-store/src/Product.js
@@ -21,6 +21,26 @@ class Product extends React.Component {
         <p>Likes: 800</p>
         <button>Add to Cart</button>
       </div>
+
+      <div>
+        <h2>Product Name: Rose</h2>
+        <img src="/images/Rose.jpg" width="100" height="100" />        
+        <p>Description: Classic and Romantic Flower</p>
+        <p>Quantity: 3000</p>
+        <p>Price: 15 Rs.</p>
+        <p>Likes: 1500</p>
+        <button>Add to Cart</button>
+      </div>
+
+      <div>
+        <h2>Product Name: Tulip</h2>
+        <img src="/images/Tulip.jpg" width="100" height="100" />        
+        <p>Description: Elegant and Graceful Flower</p>
+        <p>Quantity: 2500</p>
+        <p>Price: 8 Rs.</p>
+        <p>Likes: 1200</p>
+        <button>Add to Cart</button>
+      </div>
       </div>
     );
   }
