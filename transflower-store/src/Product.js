757c9dbf669bcfc2ea1451c50bfd587a16e5a5de
salonipawale <pawalesaloni123@gmail.com>
2026-04-03 22:13:37 +0530

    Updated Product.js file

--- transflower-store/src/Product.js
+++ transflower-store/src/Product.js
@@ -11,6 +11,16 @@ class Product extends React.Component {
         <p>Price: 10 Rs.</p>
         <p>Likes: 1000</p>
         <button>Add to Cart</button>
+
+      <div>
+        <h2>Product Name: Sunflower</h2>
+        <img src="/images/Sunflower.jpg" width="100" height="100" />        
+        <p>Description: Bright and Cheerful Flower</p>
+        <p>Quantity: 1500</p>
+        <p>Price: 12 Rs.</p>
+        <p>Likes: 800</p>
+        <button>Add to Cart</button>
+      </div>
       </div>
     );
   }
